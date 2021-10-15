4bb4e252dcc1e8e22a21885f0ec7004a9b1b5796
MateMar04 <mateo.marchisone@gmail.com>
2021-10-14 21:45:19 -0300

    improvement on setting themes

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ function show() {
             document.getElementById("lblSaludar").innerText = "Buenas noches " + name;
         }
     }
-
-
-
-
 }
 
 function clear() {
@@ -27,191 +23,37 @@ function clear() {
 }
 
 function lightTheme() {
-    document.getElementById("body").classList.add("body-light")
-    document.getElementById("body").classList.remove("body-dark")
-    document.getElementById("body").classList.remove("body-contrast")
-    document.getElementById("body").classList.remove("body-rainbow")
-
-    document.getElementById("title").classList.add("title-light")
-    document.getElementById("title").classList.remove("title-dark")
-    document.getElementById("title").classList.remove("title-contrast")
-    document.getElementById("title").classList.remove("title-rainbow")
-
-    document.getElementById("btnMostrar").classList.add("button-light")
-    document.getElementById("btnMostrar").classList.remove("button-dark")
-    document.getElementById("btnMostrar").classList.remove("button-contrast")
-    document.getElementById("btnMostrar").classList.remove("button-rainbow")
-
-    document.getElementById("btnBorrar").classList.add("button-light")
-    document.getElementById("btnBorrar").classList.remove("button-dark")
-    document.getElementById("btnBorrar").classList.remove("button-contrast")
-    document.getElementById("btnBorrar").classList.remove("button-rainbow")
-
-    document.getElementById("lblSaludar").classList.add("text-light")
-    document.getElementById("lblSaludar").classList.remove("text-dark")
-    document.getElementById("lblSaludar").classList.remove("text-contrast")
-    document.getElementById("lblSaludar").classList.remove("text-rainbow")
-
-    document.getElementById("btnLightTheme").classList.add("button-light")
-    document.getElementById("btnLightTheme").classList.remove("button-dark")
-    document.getElementById("btnLightTheme").classList.remove("button-contrast")
-    document.getElementById("btnLightTheme").classList.remove("button-rainbow")
-
-    document.getElementById("btnDarkTheme").classList.add("button-light")
-    document.getElementById("btnDarkTheme").classList.remove("button-dark")
-    document.getElementById("btnDarkTheme").classList.remove("button-contrast")
-    document.getElementById("btnDarkTheme").classList.remove("button-rainbow")
-
-    document.getElementById("btnContrastTheme").classList.add("button-light")
-    document.getElementById("btnContrastTheme").classList.remove("button-dark");
-    document.getElementById("btnContrastTheme").classList.remove("button-contrast")
-    document.getElementById("btnContrastTheme").classList.remove("button-rainbow")
-
-    document.getElementById("btnRainbowTheme").classList.add("button-light")
-    document.getElementById("btnRainbowTheme").classList.remove("button-dark");
-    document.getElementById("btnRainbowTheme").classList.remove("button-contrast")
-    document.getElementById("btnRainbowTheme").classList.remove("button-rainbow")
+    setTheme("light")
 }
 
 function darkTheme() {
-    document.getElementById("body").classList.remove("body-light")
-    document.getElementById("body").classList.add("body-dark")
-    document.getElementById("body").classList.remove("body-contrast")
-    document.getElementById("body").classList.remove("body-rainbow")
-
-    document.getElementById("title").classList.remove("title-light")
-    document.getElementById("title").classList.add("title-dark")
-    document.getElementById("title").classList.remove("title-contrast")
-    document.getElementById("title").classList.remove("title-rainbow")
-
-    document.getElementById("btnMostrar").classList.remove("button-light")
-    document.getElementById("btnMostrar").classList.add("button-dark")
-    document.getElementById("btnMostrar").classList.remove("button-contrast")
-    document.getElementById("btnMostrar").classList.remove("button-rainbow")
-
-    document.getElementById("btnBorrar").classList.remove("button-light")
-    document.getElementById("btnBorrar").classList.add("button-dark")
-    document.getElementById("btnBorrar").classList.remove("button-contrast")
-    document.getElementById("btnBorrar").classList.remove("button-rainbow")
-
-    document.getElementById("lblSaludar").classList.remove("text-light")
-    document.getElementById("lblSaludar").classList.add("text-dark")
-    document.getElementById("lblSaludar").classList.remove("text-contrast")
-    document.getElementById("lblSaludar").classList.remove("text-rainbow")
-
-    document.getElementById("btnLightTheme").classList.remove("button-light")
-    document.getElementById("btnLightTheme").classList.add("button-dark")
-    document.getElementById("btnLightTheme").classList.remove("button-contrast")
-    document.getElementById("btnLightTheme").classList.remove("button-rainbow")
-
-    document.getElementById("btnDarkTheme").classList.remove("button-light")
-    document.getElementById("btnDarkTheme").classList.add("button-dark")
-    document.getElementById("btnDarkTheme").classList.remove("button-contrast")
-    document.getElementById("btnDarkTheme").classList.remove("button-rainbow")
-
-
-    document.getElementById("btnContrastTheme").classList.remove("button-light")
-    document.getElementById("btnContrastTheme").classList.add("button-dark");
-    document.getElementById("btnContrastTheme").classList.remove("button-contrast")
-    document.getElementById("btnContrastTheme").classList.remove("button-rainbow")
-
-    document.getElementById("btnRainbowTheme").classList.remove("button-light")
-    document.getElementById("btnRainbowTheme").classList.add("button-dark");
-    document.getElementById("btnRainbowTheme").classList.remove("button-contrast")
-    document.getElementById("btnRainbowTheme").classList.remove("button-rainbow")
+    setTheme("dark")
 }
 
 function contrastTheme() {
-    document.getElementById("body").classList.remove("body-light")
-    document.getElementById("body").classList.remove("body-dark")
-    document.getElementById("body").classList.add("body-contrast")
-    document.getElementById("body").classList.remove("body-rainbow")
-
-    document.getElementById("title").classList.remove("title-light")
-    document.getElementById("title").classList.remove("title-dark")
-    document.getElementById("title").classList.add("title-contrast")
-    document.getElementById("title").classList.remove("title-rainbow")
-
-    document.getElementById("btnMostrar").classList.remove("button-light")
-    document.getElementById("btnMostrar").classList.remove("button-dark")
-    document.getElementById("btnMostrar").classList.add("button-contrast")
-    document.getElementById("btnMostrar").classList.remove("button-rainbow")
-
-    document.getElementById("btnBorrar").classList.remove("button-light")
-    document.getElementById("btnBorrar").classList.remove("button-dark")
-    document.getElementById("btnBorrar").classList.add("button-contrast")
-    document.getElementById("btnBorrar").classList.remove("button-rainbow")
-
-
-    document.getElementById("lblSaludar").classList.remove("text-light")
-    document.getElementById("lblSaludar").classList.remove("text-dark")
-    document.getElementById("lblSaludar").classList.add("text-contrast")
-    document.getElementById("lblSaludar").classList.remove("text-rainbow")
-
-    document.getElementById("btnLightTheme").classList.remove("button-light")
-    document.getElementById("btnLightTheme").classList.remove("button-dark")
-    document.getElementById("btnLightTheme").classList.add("button-contrast")
-    document.getElementById("btnLightTheme").classList.remove("button-rainbow")
-
-    document.getElementById("btnDarkTheme").classList.remove("button-light")
-    document.getElementById("btnDarkTheme").classList.remove("button-dark")
-    document.getElementById("btnDarkTheme").classList.add("button-contrast")
-    document.getElementById("btnDarkTheme").classList.remove("button-rainbow")
-
-    document.getElementById("btnContrastTheme").classList.remove("button-light")
-    document.getElementById("btnContrastTheme").classList.remove("button-dark");
-    document.getElementById("btnContrastTheme").classList.add("button-contrast")
-    document.getElementById("btnContrastTheme").classList.remove("button-rainbow")
-
-    document.getElementById("btnRainbowTheme").classList.remove("button-light")
-    document.getElementById("btnRainbowTheme").classList.remove("button-dark");
-    document.getElementById("btnRainbowTheme").classList.add("button-contrast")
-    document.getElementById("btnRainbowTheme").classList.remove("button-rainbow")
+    setTheme("contrast")
 }
 
 function rainbowTheme() {
-    document.getElementById("body").classList.remove("body-light")
-    document.getElementById("body").classList.remove("body-dark")
-    document.getElementById("body").classList.remove("body-contrast")
-    document.getElementById("body").classList.add("body-rainbow")
+    setTheme("rainbow")
+}
 
-    document.getElementById("title").classList.remove("title-light")
-    document.getElementById("title").classList.remove("title-dark")
-    document.getElementById("title").classList.remove("title-contrast")
-    document.getElementById("title").classList.add("title-rainbow")
+function setTheme(themeToSet) {
+    const components = [
+        ["body", "body"],
+        ["title", "title"],
+        ["btn_Mostrar", "button"],
+        ["btnBorrar", "button"],
+        ["lblSaludar", "text"],
+        ["btnLightTheme", "button"],
+        ["btnDarkTheme", "button"],
+        ["btnContrastTheme", "button"],
+        ["btnRainbowTheme", "button"]];
 
-    document.getElementById("btnMostrar").classList.remove("button-light")
-    document.getElementById("btnMostrar").classList.remove("button-dark")
-    document.getElementById("btnMostrar").classList.remove("button-contrast")
-    document.getElementById("btnMostrar").classList.add("button-rainbow")
-
-    document.getElementById("btnBorrar").classList.remove("button-light")
-    document.getElementById("btnBorrar").classList.remove("button-dark")
-    document.getElementById("btnBorrar").classList.remove("button-contrast")
-    document.getElementById("btnBorrar").classList.add("button-rainbow")
-
-    document.getElementById("lblSaludar").classList.remove("text-light")
-    document.getElementById("lblSaludar").classList.remove("text-dark")
-    document.getElementById("lblSaludar").classList.remove("text-contrast")
-    document.getElementById("lblSaludar").classList.add("text-rainbow")
-
-    document.getElementById("btnLightTheme").classList.remove("button-light")
-    document.getElementById("btnLightTheme").classList.remove("button-dark")
-    document.getElementById("btnLightTheme").classList.remove("button-contrast")
-    document.getElementById("btnLightTheme").classList.add("button-rainbow")
-
-    document.getElementById("btnDarkTheme").classList.remove("button-light")
-    document.getElementById("btnDarkTheme").classList.remove("button-dark")
-    document.getElementById("btnDarkTheme").classList.remove("button-contrast")
-    document.getElementById("btnDarkTheme").classList.add("button-rainbow")
-
-    document.getElementById("btnContrastTheme").classList.remove("button-light")
-    document.getElementById("btnContrastTheme").classList.remove("button-dark");
-    document.getElementById("btnContrastTheme").classList.remove("button-contrast")
-    document.getElementById("btnContrastTheme").classList.add("button-rainbow")
-
-    document.getElementById("btnRainbowTheme").classList.remove("button-light")
-    document.getElementById("btnRainbowTheme").classList.remove("button-dark");
-    document.getElementById("btnRainbowTheme").classList.remove("button-contrast")
-    document.getElementById("btnRainbowTheme").classList.add("button-rainbow")
+    const themesToSet = [themeToSet];
+    const allThemes = ["light", "dark", "rainbow", "contrast"]
+    components.forEach(element => {
+        allThemes.forEach(theme => document.getElementById(element[0]).classList.remove(element[1] + "-" + theme));
+        themesToSet.forEach(theme => document.getElementById(element[0]).classList.add(element[1] + "-" + theme))
+    });
 }
